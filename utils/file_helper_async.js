51d2a9e6170a688e8a55396d8dba3a42995063d1
Enieber Cunha <7907068+enieber@users.noreply.github.com>
2025-04-01 23:15:34 -0300

    Update file_helper_async.js

--- utils/file_helper_async.js
+++ utils/file_helper_async.js
@@ -1,7 +1,7 @@
 const fs = require("fs/promises");
 
 const read_data_async = async (filePath) => {
-  const rawJson = await fs.readFile(filePath);
+  const rawJson = await fs.readFile(filePath, "utf-8");
   const data = JSON.parse(rawJson) || [];
   return data.map((item, index) => {
     return {
